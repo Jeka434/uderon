5e0122168fe015dce960dcd34d02b1b2204edebc
Jeka434 <vasinevgeniy@yandex.ru>
2018-12-20 02:23:45 +0300

    Grand Finale!+

---
+++ html/index.php
@@ -0,0 +1,19 @@
+<?php include_once ("func.php");?>
+<!doctype html>
+<head>
+ <meta charset="UTF-8">
+ <link rel="icon" type="image/png" href="img/favicon.ico" />
+ <title>Пользователи - Uderon</title>
+</head>
+<body>
+<h2>Проверить пользователя</h2>
+<form method="post">
+ <label>Имя</label>
+ <input type="text" name="fname">
+ <label>Фамилия</label>
+ <input type="text" name="lname">
+ <input type="submit" name="pidcheck" value="Проверить" >
+ <?php $connect->close();?>
+</form>
+</body>
+</html>
