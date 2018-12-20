b503652071f8f7944a783269aa12084f5b1b4ac2
Jeka434 <vasinevgeniy@yandex.ru>
2018-12-20 23:36:02 +0300

    Set order by ID

--- html/list.php
+++ html/list.php
@@ -9,7 +9,7 @@
     $connect = new mysqli("localhost", "pidor", "password", "piddb" );
     $connect->query("SET NAMES 'utf8' ");
 
-    $users = $connect->query("SELECT * FROM pidwart");
+    $users = $connect->query("SELECT * FROM pidwart ORDER BY ID");
     echo "<p>Всего пользователей в базе: ".$users->num_rows."</p>";
     $num = 0;
     //засовываем все записи в ассоциативный массив и перебираем их
