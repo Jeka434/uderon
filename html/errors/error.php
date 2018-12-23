b0061ff624b9021471485e7926e0895a42b98c7f
Jeka434 <vasinevgeniy@yandex.ru>
2018-12-23 10:23:20 +0300

    Added error.php

---
+++ html/errors/error.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8">
+    <title>Ошибка <?= htmlspecialchars($HTTP_SERVER_VARS['QUERY_STRING']); ?> - Uderon</title>
+    <link rel="icon" type="image/png" href="img/favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="css/mainstyle.css" />
+  </head>
+  <body>
+    <div class="mainBody">
+      <div class="head">
+        <h2><?= htmlspecialchars($HTTP_SERVER_VARS['QUERY_STRING']); ?>. Похоже вы пидарок</h2>
+        <a href="/">Добавьте себя в базу</a>
+      </div>
+    </div>
+  </body>
+</html>
