74f600d9080c2e2ac0dfba57c1b5bcc9eef754db
Jeka434 <vasinevgeniy@yandex.ru>
2018-12-23 13:11:52 +0300

    Deleted fucking height: 100%;

--- html/index.php
+++ html/index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8">
     <title>База пидарков - Uderon</title>
-    <link rel="stylesheet" type="text/css" href="/styles/mainstyle.css?v=1.1" />
+    <link rel="stylesheet" type="text/css" href="/styles/mainstyle.css?v=1.2" />
   </head>
   <body>
     <div class="mainBody">
--- html/list.php
+++ html/list.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8">
     <title>Список - Uderon</title>
-    <link rel="stylesheet" type="text/css" href="/styles/mainstyle.css?v=1.1" />
+    <link rel="stylesheet" type="text/css" href="/styles/mainstyle.css?v=1.2" />
   </head>
   <body>
 <?php
