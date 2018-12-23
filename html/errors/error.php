66df86ea388cb7e2ace29e4c8c5236c8d4203de4
Jeka434 <vasinevgeniy@yandex.ru>
2018-12-23 10:28:48 +0300

    Fixed error.php

--- html/errors/error.php
+++ html/errors/error.php
@@ -2,14 +2,14 @@
 <html lang="ru">
   <head>
     <meta charset="UTF-8">
-    <title>Ошибка <?= htmlspecialchars($HTTP_SERVER_VARS['QUERY_STRING']); ?> - Uderon</title>
+    <title>Ошибка <?= htmlspecialchars($_SERVER['QUERY_STRING']); ?> - Uderon</title>
     <link rel="icon" type="image/png" href="img/favicon.ico" />
     <link rel="stylesheet" type="text/css" href="css/mainstyle.css" />
   </head>
   <body>
     <div class="mainBody">
       <div class="head">
-        <h2><?= htmlspecialchars($HTTP_SERVER_VARS['QUERY_STRING']); ?>. Похоже вы пидарок</h2>
+        <h2><?= htmlspecialchars($_SERVER['QUERY_STRING']); ?>. Похоже вы пидарок</h2>
         <a href="/">Добавьте себя в базу</a>
       </div>
     </div>
