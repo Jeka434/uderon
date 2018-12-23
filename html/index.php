2959711ec76e724db8497d52c1f5af1d007beb2a
Jeka434 <vasinevgeniy@yandex.ru>
2018-12-23 10:52:33 +0300

    Improve design

--- html/index.php
+++ html/index.php
@@ -11,8 +11,8 @@
       <div class="head">
         <h2>Проверить пользователя</h2>
         <form class="mainForm" method="post">
-          <label>Имя<input type="text" name="fname"></label>
-          <label>Фамилия<input type="text" name="lname"></label>
+          <label>Имя: <input type="text" name="fname"></label>
+          <label>Фамилия: <input type="text" name="lname"></label>
           <input type="submit" name="pidcheck" value="Проверить">
         </form>
       </div>
