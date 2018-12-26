cb5665bce1303b9cf3e4102957762380e3c0a098
Jeka434 <vasinevgeniy@yandex.ru>
2018-12-26 11:08:20 +0300

    Fixed ;

--- uderon.com/php/loginstart.php
+++ uderon.com/php/loginstart.php
@@ -6,5 +6,5 @@ if (isset($_GET['ret_name']) && in_array($_GET['ret_name'], array(
 ))) {
     $ret_name = $_GET['ret_name'];
 } else {
-    $ret_name = 'error.php?400'
+    $ret_name = 'error.php?400';
 }
